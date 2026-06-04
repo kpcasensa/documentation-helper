ef74b6c79d5e47fe20fd9296b5aac2433370a886
KP <kaypee.casensa@gmail.com>
2026-06-04 21:06:07 +0800

    Add ingestion

---
+++ ingestion.py
@@ -0,0 +1,74 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+import certifi
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_ollama import OllamaEmbeddings
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (
+    Colors, log_error, log_header, log_info,
+    log_success, log_warning
+)
+
+load_dotenv()
+
+# Configure SSL context to use certifi certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OllamaEmbeddings(
+    model="nomic-embed-text",
+)
+
+chroma = Chroma(
+    persist_directory=".chroma_db",
+    embedding_function=embeddings
+)
+
+tavily_crawl = TavilyCrawl()
+
+async def main():
+    log_header("DOCUMENTATION INGESTION PIPELINE")
+
+    log_info(
+        "TavilyCrawl: Starting to Crawl documentation from https://python.langchain.com/",
+        Colors.PURPLE
+    )
+
+    # Crawl the documentation site
+    res = tavily_crawl.invoke({
+        "url": "https://python.langchain.com/",
+        "max_depth": 1,
+        "extract_depth": "advanced",
+    })
+
+    all_docs = [
+        Document(page_content=result['raw_content'], metadata={"source": result['url']})
+        for result in res['results']
+    ]
+    log_success(
+        f"TavilyCrawl: Successfully crawled {len(all_docs)} URLs from documentation site."
+    )
+
+    # Split documents into chunks
+    log_header("DOCUMENT CHUNKING PHASE")
+    log_info(
+        f"Text Splitter: Processing {len(all_docs)} documents with 4000 chunk size and 200 overlap.",
+        Colors.YELLOW
+    )
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=4000, chunk_overlap=200
+    )
+    splitted_docs = text_splitter.split_documents(all_docs)
+    log_success(
+        f"Text Splitter: Created {len(splitted_docs)} chunks from {len(all_docs)} documents."
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
